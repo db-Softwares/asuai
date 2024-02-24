60d2cf0143da6e809dfaceb2a2cad3b49725d17a
db-Softwares <98025945+db-Softwares@users.noreply.github.com>
2024-02-25 00:23:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-__import__('pysqlite3')
+__import__('pysqlite3-binary')
 import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3-binary')
 
 
 import os
